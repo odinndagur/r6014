0fa6cb55c0cd3c2cd916da4081c698f0ce46b474
Óðinn Dagur Bjarnason <odinnd@gmail.com>
2023-02-07 07:38:17 +0000

    lol

--- src/index.ts
+++ src/index.ts
@@ -33,25 +33,25 @@ async function load() {
   );
 
 
-  const result = await worker.db.query(`
-  SELECT band.name AS band,
-  album.title AS album,
-  album.released as year
-  FROM band
-  JOIN album ON album.band_id = band.id
-  ORDER BY band.name, album.title`);
+  // const result = await worker.db.query(`
+  // SELECT band.name AS band,
+  // album.title AS album,
+  // album.released as year
+  // FROM band
+  // JOIN album ON album.band_id = band.id
+  // ORDER BY band.name, album.title`);
   window['db'] = worker.db
 
   // document.body.textContent = JSON.stringify(result);
-  document.getElementById('json')!.innerHTML = `
-  <input oninput="myFunction()" type="text" id="myInput">
-  <table id="myTable">
-  <tr>
-  <th onclick="sortTable(0)">Band</th>
-  <th onclick="sortTable(1)">Album</th>
-  <th onclick="sortTable(2)">Year</th>
-  </tr>
-  ${result.map(el => { return '<tr><td>' + el['band'] + '</td>' + '<td>' + el['album'] + '</td>' + '</td>' + '<td>' + el['year'] + '</td>' + '</tr>'; }).join('\n')}</table>`
+//   document.getElementById('json')!.innerHTML = `
+//   <input oninput="myFunction()" type="text" id="myInput">
+//   <table id="myTable">
+//   <tr>
+//   <th onclick="sortTable(0)">Band</th>
+//   <th onclick="sortTable(1)">Album</th>
+//   <th onclick="sortTable(2)">Year</th>
+//   </tr>
+//   ${result.map(el => { return '<tr><td>' + el['band'] + '</td>' + '<td>' + el['album'] + '</td>' + '</td>' + '<td>' + el['year'] + '</td>' + '</tr>'; }).join('\n')}</table>`
 }
 
 load();
